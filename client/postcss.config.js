31215a7ac4b596502fcb56cb5c173020ac737726
muukojima <muu.kojima.dev@gmail.com>
2021-05-13 22:36:08 +0900

    Add minification for postcss

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,11 +1,15 @@
 const postcssImport = require('postcss-import');
 const postcssPresetEnv = require('postcss-preset-env');
+const cssnano = require('cssnano');
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
     postcssImport(),
     tailwindcss(),
+    cssnano({
+      preset: 'default',
+    }),
     postcssPresetEnv({
       stage: 3,
     }),
